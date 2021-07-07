f22a825cd70f84aa0029cd93828d10848c5a8788
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-07 08:03:13 -0700

    shorter code

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ module.exports = {
     getPercentage: async function(value, total) {
        return (value / total) * 100 
     },
-    random: async function(options = {}){
-        let min = options.min
-        let max = options.max
-        if(!min) min = 0
-        if(!max) max = 999999**2
+    random: async function({ min = 0 , max = 999999**2}){
         return Math.random() * (max - min + 1) + min;
     },
     evaluate: async function(expression){
