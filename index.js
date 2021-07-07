a055da5e01b894bb4f971a0d64c4ab6f4c7bf875
Korabi <korabarifi53@icloud.com>
2021-07-07 23:25:24 +0200

    Started using classes.

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-module.exports = {
-    /**
-     Get the percentage of a number from a total value
-    **/
+const functions = {
     getPercentage: async function(value, total) {
        return (value / total) * 100 
     },
-    /**
-      Get a random number between two numbers
-    **/
     random: async function({ min = 0 , max = 999999**2}){
         return Math.random() * (max - min + 1) + min;
     },
-    /**
-      Evaluate a mathmatical expression
-    **/
     evaluate: async function(expression){
         const r = /[a-zA-Z]/g
         const v = String(expression.toLowerCase().replace("pi", Math.PI))
         if(r.test(v) == true) throw new SyntaxError("Expression shall not contain a string value.")
         return eval(v)
     },
-    /**
-      Check if a number is a multiple of another
-    **/
     multipleOf: async function(basenum,num){
         return num % basenum ? false : true
     },
-    /**
-     Get the squareroot of a number
-    **/
     root: async function(number) {
         return Math.sqrt(number)
     } 
+}
+
+
+class MathClient {
+super(){
+    this.root = functions.root
+    this.multipleOf = functions.multipleOf
+    this.evaluate = functions.evaluate
+    this.random = functions.random
+    this.getPercentage = functions.getPercentage
+}
 }
