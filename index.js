781553d35031f8bd032d95d11779ca4a32fab5d5
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-08 10:28:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const functions = {
     format: async function(number){
-       return parseInt(number.toString().split("").reverse().join("").match(new RegExp('.{1,' + 3 + '}', 'g')).reverse().join(","));
+       return number.toString().split("").reverse().join("").match(new RegExp('.{1,' + 3 + '}', 'g')).reverse().join(",");
     },
     getPercentage: async function(value, total) {
        return (value / total) * 100 
