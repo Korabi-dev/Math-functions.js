c74867455dca2b0b79259d9dd5e98e0aa6b66f0c
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-07-09 00:03:00 +0200

    Fixed async functions thing

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const functions = {
-    format: async function(number){
+    format: function(number){
        return number.toString().split("").reverse().join("").match(new RegExp('.{1,' + 3 + '}', 'g')).reverse().join(",");
     },
-    getPercentage: async function(value, total) {
+    getPercentage:  function(value, total) {
        return (value / total) * 100 
     },
-    random: async function({ min = 0 , max = 999999**2}){
+    random: function({ min = 0 , max = 999999**2}){
         return Math.random() * (max - min + 1) + min;
     },
-    evaluate: async function(expression){
+    evaluate: function(expression){
         const r = /[a-zA-Z]/g
         const v = String(expression.toLowerCase().replace("pi", Math.PI))
         if(r.test(v) == true) throw new SyntaxError("Expression shall not contain a string value.")
         return eval(v)
     },
-    multipleOf: async function(basenum,num){
+    multipleOf: function(basenum,num){
         return num % basenum ? false : true
     },
-    root: async function(number) {
+    root:  function(number) {
         return Math.sqrt(number)
     } 
 }
